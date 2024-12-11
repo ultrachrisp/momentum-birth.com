d04865026ae090628ee65ebead82f14be59c190f
Chris Pyke <info@chrispyke.com>
2024-12-11 15:42:29 +0100

    build: onscroll template script in place

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,3 +10,41 @@ window.onscroll = function () {
     nav.classList.remove('top-nav');
   }
 }
+
+// (function( global ) {
+//     var Module =
+//         (function() {
+
+//             var data = "secret";
+//             return {
+//                 scrollTop: 0,
+//                 header: document.getElementById('headerImage'),
+//                 title: document.getElementById('homeTitle'),
+//                 nav: document.getElementById('nav') || null,
+
+//                 setHeight: function( value, elem ) {
+//                     return (elem.setAttribute( 'style', 'height:'+value+'px' ));
+//                 }
+//             };
+//   })();
+
+//     global.Module = Module;
+//     // Set the initial state
+//     if(Module.header){ Module.setHeight(window.innerHeight, Module.header); }
+//     if(Module.nav){ Module.nav.classList.remove("active"); }
+
+//     window.addEventListener('scroll', function() {
+//         Module.scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+//         if(Module.scrollTop >= 5) {
+//             if(Module.nav){ Module.nav.classList.add("active"); }
+//             Module.title.classList.remove('active');
+//         } else {
+//             if(Module.nav){ Module.nav.classList.remove("active"); }
+//             Module.title.classList.add('active');
+//         }
+
+//         return Module.scrollTop;
+//     });
+
+// })( this );
