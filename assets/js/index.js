7c1537199e3ace4ea7e99693733369463d4b1383
Chris Pyke <info@chrispyke.com>
2024-12-12 20:51:02 +0100

    build: most recent changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
 const nav = document.querySelector('.nav');
-const logo = document.getElementById('logo');
+// const logo = document.getElementById('logo');
 const homepage = document.querySelector('.homepage');
 
 window.onscroll = function () {
-  if (window.scrollY > 10) {
-    logo.classList.remove('active');
-  } else {
-    logo.classList.add('active');
-  }
+  // if (window.scrollY > 10) {
+  //   logo.classList.remove('active');
+  // } else {
+  //   logo.classList.add('active');
+  // }
 
   if (window.scrollY > (homepage.offsetHeight - nav.offsetHeight)) {
     nav.classList.remove('bottom-nav');
