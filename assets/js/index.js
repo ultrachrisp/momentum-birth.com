3de4f17ec0cba261d2f1c50fd5228b963c882cfb
Chris Pyke <info@chrispyke.com>
2024-12-12 17:37:53 +0100

    build: logo and scripts in place

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,15 @@
-var main = document.querySelector('.homepage');
-var nav = document.querySelector('.nav');
+const nav = document.querySelector('.nav');
+const logo = document.getElementById('logo');
+const homepage = document.querySelector('.homepage');
 
 window.onscroll = function () {
-  if (window.scrollY > (main.offsetHeight - nav.offsetHeight)) {
+  if (window.scrollY > 10) {
+    logo.classList.remove('active');
+  } else {
+    logo.classList.add('active');
+  }
+
+  if (window.scrollY > (homepage.offsetHeight - nav.offsetHeight)) {
     nav.classList.remove('bottom-nav');
     nav.classList.add('top-nav');
   } else {
@@ -10,41 +17,3 @@ window.onscroll = function () {
     nav.classList.remove('top-nav');
   }
 }
-
-// (function( global ) {
-//     var Module =
-//         (function() {
-
-//             var data = "secret";
-//             return {
-//                 scrollTop: 0,
-//                 header: document.getElementById('headerImage'),
-//                 title: document.getElementById('homeTitle'),
-//                 nav: document.getElementById('nav') || null,
-
-//                 setHeight: function( value, elem ) {
-//                     return (elem.setAttribute( 'style', 'height:'+value+'px' ));
-//                 }
-//             };
-//   })();
-
-//     global.Module = Module;
-//     // Set the initial state
-//     if(Module.header){ Module.setHeight(window.innerHeight, Module.header); }
-//     if(Module.nav){ Module.nav.classList.remove("active"); }
-
-//     window.addEventListener('scroll', function() {
-//         Module.scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-
-//         if(Module.scrollTop >= 5) {
-//             if(Module.nav){ Module.nav.classList.add("active"); }
-//             Module.title.classList.remove('active');
-//         } else {
-//             if(Module.nav){ Module.nav.classList.remove("active"); }
-//             Module.title.classList.add('active');
-//         }
-
-//         return Module.scrollTop;
-//     });
-
-// })( this );
